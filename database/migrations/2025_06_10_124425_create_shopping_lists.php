3350602b95e06cf29a3d950350230261c5d90528
Hjordten <andr314e@stud.kea.dk>
2025-06-16 15:59:25 +0200

    added formating with slice

--- database/migrations/2025_06_10_124425_create_shopping_lists.php
+++ database/migrations/2025_06_10_124425_create_shopping_lists.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_favorite')->default(false);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
