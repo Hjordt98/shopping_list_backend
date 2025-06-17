fa657c04757730197574393bae8eadfe1735939a
Hjordten <andr314e@stud.kea.dk>
2025-06-17 10:45:47 +0200

    a

--- app/Models/ShoppingListItems.php
+++ app/Models/ShoppingListItems.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class ShoppingListItems extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'quantity', 'is_checked', 'shopping_list_id'];
+    protected $fillable = ['name', 'quantity', 'is_checked', 'shopping_list_id', 'category'];
 
     public function shoppingList()
     {
