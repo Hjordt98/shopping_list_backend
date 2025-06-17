3535d2027f85b873dd922e303cb2a82e834b41e3
Hjordten <andr314e@stud.kea.dk>
2025-06-17 10:20:57 +0200

    a

--- database/migrations/2025_06_10_124444_shopping_list_items.php
+++ database/migrations/2025_06_10_124444_shopping_list_items.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->integer('quantity')->default(1);
             $table->boolean('is_checked')->default(false);
             $table->foreignId('shopping_list_id')->constrained('shopping_lists')->onDelete('cascade');
+            $table->string('category')->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
